1b76b5493f7d982198d2c143c293c6201ecb8120
Berkay Tamer <berkay_tamer1907@hotmail.com>
2016-08-03 15:05:57 -0600

    added lessons

---
+++ 0: Introduction to JavaScript/Getting to know you/0:what-is-your-name.js
@@ -0,0 +1,4 @@
+var berks = "Berkay";
+
+document.write(berks); //Executes your code on the page
+console.log(berks);  //Executes your code on the console
---
+++ 0: Introduction to JavaScript/Getting to know you/1:discover-the-length.js
@@ -0,0 +1,4 @@
+var berks = "Berkay".length;
+
+document.write(berks); //Executes your code on the page
+console.log(berks);  //Executes your code on the console
